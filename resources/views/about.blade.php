4dca1abd43d71323b43e571d8cc61a78feb924bb
DmitriyNemov <dmitrijnemov4@gmail.com>
2023-06-14 17:03:59 +0400

    third commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layout\main')
+@section('content')
+<div>
+    this is about page
+</div>
+@endsection
---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,6 @@
+@extends('layout\main')
+@section('content')
+<div>
+    this is contacts page
+</div>
+@endsection
